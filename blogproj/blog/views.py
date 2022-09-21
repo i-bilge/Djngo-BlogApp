3edf148debecc39a10c742fe3d22f82fb605881a
i-bilge <iismailbilge@gmail.com>
2022-09-21 11:56:05 +0200

    posts date in views added to home temolate.

--- blogproj/blog/views.py
+++ blogproj/blog/views.py
@@ -1,8 +1,33 @@
+from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Peyami Safa',
+        'title': '9. Hariciye Kogusu',
+        'content': 'This is the content of book1',
+        'date_posted': '21th September, 2022',
+    },
+    {
+        'author': 'Jane Austen',
+        'title': 'Pride and Prejudice',
+        'content': 'This is the content of book2',
+        'date_posted': '21th September, 1813',
+    },
+    {
+        'author': 'José Saramago',
+        'title': 'Blindness',
+        'content': 'This is the content of book3',
+        'date_posted': '21th September, 1995',
+    },
+]
+
 # Create your views here.
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts':posts
+    }
+    return render(request, 'blog/home.html', context)
 def about(request):
     return render(request, 'blog/about.html')

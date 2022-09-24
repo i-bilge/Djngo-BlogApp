531cee0b518e851afef53772d1b373c55ba91543
i-bilge <iismailbilge@gmail.com>
2022-09-24 13:15:34 +0200

    views are added.

--- blogproj/blog/models.py
+++ blogproj/blog/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=200, null=True)
     content = models.TextField(null=True, blank=True)
     date_posted = models.DateTimeField(auto_now_add=True)
--- blogproj/blog/views.py
+++ blogproj/blog/views.py
@@ -1,6 +1,18 @@
 from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import FormView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import login
+
+from .models import Post
+from django.urls import reverse_lazy
 
 posts = [
     {
@@ -31,3 +43,50 @@ def home(request):
     return render(request, 'blog/home.html', context)
 def about(request):
     return render(request, 'blog/about.html', {'title':"About Page"})
+
+# -------------------------------------------------
+class PostList(LoginRequiredMixin, ListView):
+    model = Post
+    context_object_name = 'post'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['post'] = context['post'].filter(user=self.request.user)
+        return context
+
+class PostDetail(LoginRequiredMixin, DetailView):
+    model = Post
+    context_object_name = 'post'
+    
+class PostCreate(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title','content']
+    success_url = reverse_lazy('post')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(PostCreate, self).form_valid(form)
+
+class PostUpdate(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['title','content']
+    success_url = reverse_lazy("post")
+
+class PostDelete(LoginRequiredMixin, DeleteView):
+    model = Post
+    context_object_name = 'post'
+    success_url = reverse_lazy("post")
+
+class CustomLoginView(LoginView):
+    template_name = 'blog/login.html'
+    fields = "__all__"
+    redirect_authenticated_user: False
+
+    def get_success_url(self):
+        return reverse_lazy('post')
+
+class RegisterPage(FormView):
+    template_name = 'blog/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('post')

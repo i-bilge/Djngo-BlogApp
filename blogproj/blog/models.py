c182c2943e1c6207d9980cec84b81ba32a1ae10c
i-bilge <iismailbilge@gmail.com>
2022-09-24 12:54:10 +0200

    Post model is added.

--- blogproj/blog/models.py
+++ blogproj/blog/models.py
@@ -1,3 +1,18 @@
+from turtle import title
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=200, null=True)
+    content = models.TextField(null=True, blank=True)
+    date_posted = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['date_updated']
+    

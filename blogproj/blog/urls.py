6d4f0ef85107bf7c95d5b91788d8b52dc659f8fe
i-bilge <iismailbilge@gmail.com>
2022-09-24 13:46:16 +0200

    post list template of home page is added.

--- blogproj/blog/urls.py
+++ blogproj/blog/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-from . import views
+from .views import PostList, PostDetail, PostCreate, PostUpdate, PostDelete, CustomLoginView, RegisterPage
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('', views.home, name="blog-home"),
-    path('about/', views.about, name="blog-about"),
+    path('', PostList.as_view(), name='post'),
+    path('post-create/', PostCreate.as_view(), name='post-create'),
+    path('post/<int:pk>/', PostDetail.as_view(), name= 'posts-detail'),
+    path('post-update/<int:pk>/', PostUpdate.as_view(), name= 'posts-update'),
+    path('post-delete/<int:pk>/', PostDelete.as_view(), name= 'posts-delete'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', RegisterPage.as_view(), name='register'),
 ]
